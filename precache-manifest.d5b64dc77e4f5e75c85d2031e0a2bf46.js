17baaa230f939187bd55726a439623e06fc5c86d
tbinhminh <tbinhminh1109@gmail.com>
2020-05-24 12:49:04 +0700

    Updates

---
+++ precache-manifest.d5b64dc77e4f5e75c85d2031e0a2bf46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d54ae0453cde88212859d4a49d9124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298746ab64b447376345",
+    "url": "/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "33797f7aabf858739a5e",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "298746ab64b447376345",
+    "url": "/static/js/2.abb1c04e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.abb1c04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33797f7aabf858739a5e",
+    "url": "/static/js/main.84c294f9.chunk.js"
+  },
+  {
+    "revision": "bece11a3e7c503135f51",
+    "url": "/static/js/runtime-main.1b70d7ff.js"
+  }
+]);
